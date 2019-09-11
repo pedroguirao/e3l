17965dbcf2317147fe468c0d6d617f5eda570b55
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2019-09-11 05:21:16 +0000

    v final

--- D-E3L/models/d_e3l.py
+++ D-E3L/models/d_e3l.py
@@ -7,9 +7,16 @@ class descarga_e3l(models.Model):
     _name = 'descarga.e3l'
     _description='Descarga E3L'
         
-    x_e3l_id = fields.Many2one('x_dis',string='E3L Descargable')
-    x_name = fields.Char()
-    x_anexo = fields.Binary() 
+    x_e3l_id = fields.Many2one(
+        'x_dis', 
+        string='E3L Descargable'
+    )
+    x_name = fields.Char(
+        string='Nombre Di asociado',
+    )
+    x_anexo = fields.Binary(
+        string='Anexo E3L',
+                           ) 
             
     @api.constrains('x_e3l_id')
     def genera_anexo(self):
